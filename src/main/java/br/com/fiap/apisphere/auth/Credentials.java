d980ceab82d0d9dee3f3e8cec5c61dabc5f179f3
Thiago Zupelli <thiago_zupelli@hotmail.com>
2024-08-19 21:07:46 -0300

    Added login

---
+++ src/main/java/br/com/fiap/apisphere/auth/Credentials.java
@@ -0,0 +1,5 @@
+package br.com.fiap.apisphere.auth;
+
+public record Credentials(String email, String password) {
+
+}
--- src/main/java/br/com/fiap/apisphere/user/UserRepository.java
+++ src/main/java/br/com/fiap/apisphere/user/UserRepository.java
@@ -1,7 +1,9 @@
 package br.com.fiap.apisphere.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
